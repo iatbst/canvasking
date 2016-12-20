1c28e2af10a12ab626eabe20650cad8ec319f3da
Josh Yang <jyang@agari.com>
2016-12-20 15:33:44 -0800

    Finish layouts for item create page

--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,18 @@ ActiveRecord::Schema.define(version: 20161218190503) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "items", force: :cascade do |t|
+    t.integer  "product_id"
+    t.integer  "width"
+    t.integer  "height"
+    t.integer  "quantity"
+    t.decimal  "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "items", ["product_id"], name: "index_items_on_product_id", using: :btree
+
   create_table "products", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
@@ -40,4 +52,5 @@ ActiveRecord::Schema.define(version: 20161218190503) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "items", "products"
 end
