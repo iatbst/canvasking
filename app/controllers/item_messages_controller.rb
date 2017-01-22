2bdf9610de0a2b8e7fff12b4b0df1dfcfdc0e33a
Josh Yang <jyang@agari.com>
2017-01-21 16:17:17 -0800

    minor update

--- app/controllers/item_messages_controller.rb
+++ app/controllers/item_messages_controller.rb
@@ -6,14 +6,16 @@ class ItemMessagesController < ApplicationController
     @message = @item.item_messages.create(message_params)
     @item.update(item_params)
     flash[:notice] = "Thanks for your feedbacks !"
-    if @item.rate > 4
-      flash[:rate] = "We feel so great today you like our product :)"
-    elsif @item.rate == 3
-      flash[:rate] = "We will improve our product for you !"
-    else
-      flash[:rate] = "We feel sorry to let you down, please send us on support@canvasking.com for replacement or refund."
+    if !@item.rate.nil?
+      if @item.rate > 4
+        flash[:rate] = "We feel so great today you like our product :)"
+      elsif @item.rate == 3
+        flash[:rate] = "We will improve our product for you !"
+      else
+        flash[:rate] = "We feel sorry to let you down, please send us on support@canvasking.com for replacement or refund."
+      end
     end
-  
+    
     redirect_to orders_path
   end
  
