7b881f025231961b4d2595c601bd5b04adcda0af
Josh Yang <jyang@agari.com>
2017-03-30 02:04:42 -0700

    Generate new user coupon for new registrated user

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,8 @@ Rails.application.routes.draw do
   get 'pricing/index'
   get 'pricing/query_price'
 
-  devise_for :users, :controllers => { :sessions => "custom_sessions", :registrations => 'registrations' }
+  devise_for :users, :controllers => { :sessions => "custom_sessions", :registrations => "custom_registrations"}
+  
   get 'welcome/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
