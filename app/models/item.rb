edca7e5b7ecd60b6e917804d94d1e223a3e5fa0c
Josh Yang <jyang@agari.com>
2017-04-20 10:33:33 -0700

    - remove canvas frame option from UI ( not reliable for long distance ) - remove canvas depth 1.5 inch option - add coupon reminder in item new summary section

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,7 +11,8 @@ class Item < ActiveRecord::Base
   validates :product_id, presence: {message: "Please choose a product"}, if: :image_is_uploaded?
   validates :size, presence: {message: "Please choose a size"}, if: :product_is_selected?
   validates :canvas_depth, presence: {message: "Please choose a depth"}, if: :canvas_is_selected?
-  validates :canvas_frame, presence: {message: "Please choose a frame or no frame"}, if: :canvas_is_selected?
+  # No frame for canvas now
+  #validates :canvas_frame, presence: {message: "Please choose a frame or no frame"}, if: :canvas_is_selected?
   validates :image, presence: {message: "Please upload a image"}, if: :image_is_not_uploaded?
   validates :frame_id, presence: {message: "Please choose a frame"}, if: :frame_is_selected?
   validates :mat, presence: {message: "Please choose a mat"}, if: :frame_is_selected?
